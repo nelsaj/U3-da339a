c752952f605e2ecd06d1d0e157e2598fb558d8ac
Elliot Collins <150339930+itsDipsyProjects@users.noreply.github.com>
2024-12-11 12:50:55 +0100

    Fixade javadoc för cake och filling classerna

--- model/Cake.java
+++ model/Cake.java
@@ -1,54 +1,96 @@
+/**
+ * Klassen Cake är en class som ärver från Classen Product men med storlek och fyllningar som enskilda attribute.
+ * Det finns klass spesifika metoder för att beräkna priset på tårtan och hämta dess data.
+ *
+ * @author Elliot Collins
+ * @author Nelly Sajland
+ */
 package model;
 
-public class Cake extends Product{
+public class Cake extends Product {
     private Filling[] fillings;
     private int size;
-    
 
-    // default values
-    //TODO: felhantering för värden i setters
-    public Cake(){
+    /**
+     * konstruktorn som initialiserar ett Cake-objekt med standardvärden.
+     */
+    public Cake() {
         super(); 
         this.setFillings(new Filling[2]);
         this.setSize(this.size);
     }
 
-    //TODO: felhantering för värden i setters
-    public Cake(Filling[] fillings, int size, String name){
-       super.setName(name);
-       this.setFillings(fillings);
-       this.setSize(size);
-       this.setPrice(calculatePrice());
+    /**
+     * Konstruktor för att initialisera ett Cake-objekt med specifika fyllningar,storlek och namn.
+     *
+     * @param fillings en array av Fillings
+     * @param size tårtans storlek
+     * @param name tårtans namn
+     */
+    public Cake(Filling[] fillings, int size, String name) {
+        super.setName(name);
+        this.setFillings(fillings);
+        this.setSize(size);
+        this.setPrice(calculatePrice());
     }
 
+    /**
+     * Hämtar tårtans storlek.
+     *
+     * @return tårtans storlek
+     */
     public int getSize() {
         return size;
     }
 
+    /**
+     * Hämtar tårtans fyllningar.
+     *
+     * @return en array av Fillings
+     */
     public Filling[] getFillings() {
         return fillings;
     }
 
+    /**
+     * Sätter tårtans fyllningar.
+     *
+     * @param fillings en array av Fillings
+     */
     public void setFillings(Filling[] fillings) {
         this.fillings = fillings;
     }
 
+    /**
+     * Sätter tårtans storlek.
+     *
+     * @param size storleken att sätta för tårtan
+     */
     public void setSize(int size) {
         this.size = size;
     }
 
+    /**
+     * Returnerar en text av cakes olika attribute som ska användas för listan av orders.
+     *
+     * @return en sträng som beskriver en cake
+     */
     @Override
-    public String toString(){
-        String cakeText = super.toString() + " Size: " + this.size + " Fillings: "; 
+    public String toString() {
+        String cakeText = super.toString() + " Storlek: " + this.size + " Fyllningar: "; 
         for (int i = 0; i < fillings.length; i++) {
             cakeText += fillings[i] + ", ";    
         } 
-
         return cakeText;
     }
 
+    /**
+     * Beräknar priset på tårtan baserat på dess fyllningar och storlek.
+     *
+     * @return det beräknade priset på tårtan
+     */
     @Override  
-    public int calculatePrice(){
+    public int calculatePrice() {
         int price = 0;
         for (int i = 0; i < fillings.length; i++) {
             price += fillings[i].getFillingPrice();
--- model/Filling.java
+++ model/Filling.java
@@ -1,8 +1,17 @@
 package model;
-
+/**
+ * Enumen Filling är för att beskriva de olika fyllningar en tårta kan ha.
+ *
+ * @author Elliot Collins
+ * @author Nelly Sajland
+ */
 public enum Filling {
     Vanilla(30), Choclate(40), Strawberry(50), Coconut(60), MixedBerries(70), Unknown(0);
     private int price;
+    /**
+     * Enum konstruktör som initialiserar pris attributet för en viss Enum literal.
+     * @param price
+     */
     Filling(int price){
         this.price = price;
     }
